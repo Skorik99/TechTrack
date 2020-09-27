33dd21e4b8497b0572e778ef8cafba40e6b00d91
Skorik99 <45143958+Skorik99@users.noreply.github.com>
2020-09-27 12:01:40 +0300

    Add files via upload

---
+++ Task1 Quadratic_equation/main.c
@@ -0,0 +1,55 @@
+#include "Quadratic_Solve.h"
+#include <stdio.h>
+#include <time.h>
+#include <conio.h>
+
+/**
+ * @file main.c
+ *
+ * @brief Программа-интерфейс для решения квадратного уравнения
+ *
+ * @author Скорик Сергей, студент МФТИ, группа Б05-875, базовая кафедра -- ИСП
+ *
+ * @date 9/26/2020
+ */
+
+
+int main()
+{
+    printf ("This is quadratic equation solver!\n");
+    printf ("Please, input a, b, c in format:a b c\n");
+
+
+    double a = 0, b = 0, c = 0;
+    char str[64];
+    fgets(str, 63, stdin);
+    while(!Check_and_parse_input(str, &a, &b, &c)) {
+        printf("Incorrect input. Try it again. Enter 3 numbers separated by a space.\n");
+        fgets(str, 63, stdin);
+    }
+    double x1 = 0, x2 = 0;
+    int rootNumber = QuadrSolve (a, b, c, &x1, &x2);
+
+    switch (rootNumber)
+    {
+    case 0:
+        printf ("No roots!\n");
+        break;
+    case 1:
+        printf ("%lf is the only root of the given equation\n", x1);
+        break;
+    case 2:
+        printf ("%lf, %lf are roots of the given equation\n", x1, x2);
+        break;
+    case INF_ROOTS:
+        printf ("Ooh! Infinite number of roots!\n");
+        break;
+    default:
+        printf ("Something went wrong...\n");
+    }
+
+    getch(); ///Wait before close
+
+
+    return 0;
+}
